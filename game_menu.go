9701ccfea6a19a890bdabcf586c59568183b7db6
sidav <sidav94@gmail.com>
2022-01-22 17:23:39 +0300

    Removes menu values reset.

--- game_menu.go
+++ game_menu.go
@@ -7,9 +7,10 @@ import (
 	"time"
 )
 
-func showGameMenu() {
-	cursor := 0
-	valuesData := [][]int{
+var (
+	menuPressDelay time.Duration = 100
+
+	menuValuesData = [][]int{
 		// {value, min, max, step}
 		{0, 0, 25, 1},
 		{10, 2, 100, 1},
@@ -22,7 +23,7 @@ func showGameMenu() {
 		{10, 0, MAP_H*MAP_W, 2},
 	}
 
-	entries := []string{
+	menuEntries = []string{
 		"INITIAL ENEMIES       <%d>",
 		"MAX TANKS ON MAP      <%d>",
 		"TOTAL TANKS PER GAME  <%d>",
@@ -33,27 +34,32 @@ func showGameMenu() {
 		"DESIRED WOODS         <%d>",
 		"DESIRED WATER         <%d>",
 	}
-	colorText := color.RGBA{
+	colorText = color.RGBA{
 		R: 255,
 		G: 255,
 		B: 255,
 		A: 255,
 	}
-	colorCursor := color.RGBA{
+	colorCursor = color.RGBA{
 		R: 255,
 		G: 0,
 		B: 0,
 		A: 255,
 	}
+)
+
+func showGameMenu() {
+	cursor := 0
+
 	for {
 		rl.BeginDrawing()
 		rl.ClearBackground(color.RGBA{0, 0, 0, 255})
 
-		for i := range entries {
+		for i := range menuEntries {
 			if i == cursor {
-				rl.DrawText(fmt.Sprintf(entries[i], valuesData[i][0]), 0, int32(i*(TEXT_SIZE+TEXT_MARGIN)), TEXT_SIZE, colorCursor)
+				rl.DrawText(fmt.Sprintf(menuEntries[i], menuValuesData[i][0]), 0, int32(i*(TEXT_SIZE+TEXT_MARGIN)), TEXT_SIZE, colorCursor)
 			} else {
-				rl.DrawText(fmt.Sprintf(entries[i], valuesData[i][0]), 0, int32(i*(TEXT_SIZE+TEXT_MARGIN)), TEXT_SIZE, colorText)
+				rl.DrawText(fmt.Sprintf(menuEntries[i], menuValuesData[i][0]), 0, int32(i*(TEXT_SIZE+TEXT_MARGIN)), TEXT_SIZE, colorText)
 			}
 		}
 		rl.EndDrawing()
@@ -61,27 +67,27 @@ func showGameMenu() {
 		if rl.IsKeyPressed(rl.KeyUp) {
 			cursor--
 			if cursor < 0 {
-				cursor = len(valuesData) - 1
+				cursor = len(menuValuesData) - 1
 			}
 
 		}
 		if rl.IsKeyDown(rl.KeyLeft) {
-			valuesData[cursor][0] -= valuesData[cursor][3]
-			if valuesData[cursor][0] < valuesData[cursor][1] {
-				valuesData[cursor][0] = valuesData[cursor][1]
+			menuValuesData[cursor][0] -= menuValuesData[cursor][3]
+			if menuValuesData[cursor][0] < menuValuesData[cursor][1] {
+				menuValuesData[cursor][0] = menuValuesData[cursor][1]
 			}
-			time.Sleep(150 * time.Millisecond)
+			time.Sleep(menuPressDelay * time.Millisecond)
 		}
 		if rl.IsKeyDown(rl.KeyRight) {
-			valuesData[cursor][0] += valuesData[cursor][3]
-			if valuesData[cursor][0] > valuesData[cursor][2] {
-				valuesData[cursor][0] = valuesData[cursor][2]
+			menuValuesData[cursor][0] += menuValuesData[cursor][3]
+			if menuValuesData[cursor][0] > menuValuesData[cursor][2] {
+				menuValuesData[cursor][0] = menuValuesData[cursor][2]
 			}
-			time.Sleep(150 * time.Millisecond)
+			time.Sleep(menuPressDelay * time.Millisecond)
 		}
 		if rl.IsKeyPressed(rl.KeyDown) {
 			cursor++
-			if cursor == len(valuesData) {
+			if cursor == len(menuValuesData) {
 				cursor = 0
 			}
 		}
@@ -97,11 +103,11 @@ func showGameMenu() {
 
 	}
 	gameMap = &battlefield{
-		initialEnemiesCount:               valuesData[0][0],
-		MaxTanksOnMap:                     valuesData[1][0],
-		totalTanksRemainingToSpawn:        valuesData[2][0],
-		chanceToSpawnEnemyEachTickOneFrom: valuesData[3][0],
-		numFactions:                       valuesData[4][0],
+		initialEnemiesCount:               menuValuesData[0][0],
+		MaxTanksOnMap:                     menuValuesData[1][0],
+		totalTanksRemainingToSpawn:        menuValuesData[2][0],
+		chanceToSpawnEnemyEachTickOneFrom: menuValuesData[3][0],
+		numFactions:                       menuValuesData[4][0],
 	}
-	gameMap.init(valuesData[5][0], valuesData[6][0], valuesData[7][0], valuesData[8][0])
+	gameMap.init(menuValuesData[5][0], menuValuesData[6][0], menuValuesData[7][0], menuValuesData[8][0])
 }
