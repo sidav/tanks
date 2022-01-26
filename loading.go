dcb8e5cfff01126374529fdb0bbc4c47f0a71724
sidav <sidav94@gmail.com>
2022-01-26 04:25:30 +0300

    Adds internal sprite resize.

--- loading.go
+++ loading.go
@@ -18,29 +18,29 @@ var (
 
 func loadImageResources() {
 	var leftXForTank = 0
-	tankAtlaces[TANK_T1] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*0, 16, true, 2)
-	tankAtlaces[TANK_T2] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*1, 16, true, 2)
-	tankAtlaces[TANK_T3] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*2, 16, true, 2)
-	tankAtlaces[TANK_T4] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*3, 16, true, 2)
-	tankAtlaces[TANK_T5] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*4, 16, true, 2)
-	tankAtlaces[TANK_T6] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*5, 16, true, 2)
-	tankAtlaces[TANK_T7] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*6, 16, true, 2)
-	tankAtlaces[TANK_T8] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*7, 16, true, 2)
+	tankAtlaces[TANK_T1] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*0, 16, 16, 2, true)
+	tankAtlaces[TANK_T2] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*1, 16, 16, 2, true)
+	tankAtlaces[TANK_T3] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*2, 16, 16, 2, true)
+	tankAtlaces[TANK_T4] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*3, 16, 16, 2, true)
+	tankAtlaces[TANK_T5] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*4, 16, 16, 2, true)
+	tankAtlaces[TANK_T6] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*5, 16, 16, 2, true)
+	tankAtlaces[TANK_T7] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*6, 16, 16, 2, true)
+	tankAtlaces[TANK_T8] = CreateAtlasFromFile("tanks.png", leftXForTank, 16*7, 16, 16, 2, true)
 	tankAtlaces[TANK_BOSS] = createAtlasFromRandomGenerated()
 
-	tileAtlaces["WALL"] = CreateAtlasFromFile("sprites.png", 16*0, 16*0, 16, false, 5)
-	tileAtlaces["ARMORED_WALL"] = CreateAtlasFromFile("sprites.png", 16*0, 16*1, 16, false, 5)
-	tileAtlaces["WATER"] = CreateAtlasFromFile("sprites.png", 16*0, 16*3, 16, false, 2)
-	tileAtlaces["WOOD"] = CreateAtlasFromFile("sprites.png", 16*1, 16*2, 16, false, 1)
-	tileAtlaces["ICE"] = CreateAtlasFromFile("sprites.png", 16*2, 16*2, 16, false, 1)
-	tileAtlaces["HQ"] = CreateAtlasFromFile("sprites.png", 16*3, 16*2, 16, false, 1)
+	tileAtlaces["WALL"] = CreateAtlasFromFile("sprites.png", 16*0, 16*0, 16, 16, 5, false)
+	tileAtlaces["ARMORED_WALL"] = CreateAtlasFromFile("sprites.png", 16*0, 16*1, 16, 16, 5, false)
+	tileAtlaces["WATER"] = CreateAtlasFromFile("sprites.png", 16*0, 16*3, 16, 16, 2, false)
+	tileAtlaces["WOOD"] = CreateAtlasFromFile("sprites.png", 16*1, 16*2, 16, 16, 1, false)
+	tileAtlaces["ICE"] = CreateAtlasFromFile("sprites.png", 16*2, 16*2, 16, 16, 1, false)
+	tileAtlaces["HQ"] = CreateAtlasFromFile("sprites.png", 16*3, 16*2, 16, 16, 1, false)
 
-	projectileAtlaces[PROJ_BULLET] = CreateAtlasFromFile("projectiles.png", 0, 0, 8, true, 1)
-	projectileAtlaces[PROJ_ROCKET] = CreateAtlasFromFile("projectiles.png", 0, 8, 8, true, 1)
+	projectileAtlaces[PROJ_BULLET] = CreateAtlasFromFile("projectiles.png", 0, 0, 8, 8, 1, true)
+	projectileAtlaces[PROJ_ROCKET] = CreateAtlasFromFile("projectiles.png", 0, 8, 8, 8, 1, true)
 
-	effectAtlaces[EFFECT_EXPLOSION] = CreateAtlasFromFile("sprites.png", 16*0, 16*6, 16, false, 3)
-	effectAtlaces[EFFECT_BIG_EXPLOSION] = CreateAtlasFromFile("sprites.png", 16*3, 16*6, 16*2, false, 2)
-	effectAtlaces[EFFECT_SPAWN] = CreateAtlasFromFile("sprites.png", 16*0, 16*4, 16, false, 4)
+	effectAtlaces[EFFECT_EXPLOSION] = CreateAtlasFromFile("sprites.png", 16*0, 16*6, 16, 16, 3, false)
+	effectAtlaces[EFFECT_BIG_EXPLOSION] = CreateAtlasFromFile("sprites.png", 16*3, 16*6, 32, 32, 2, false)
+	effectAtlaces[EFFECT_SPAWN] = CreateAtlasFromFile("sprites.png", 16*0, 16*4, 16, 16, 4, false)
 	// effectAtlaces["SPAWN"] = CreateAtlasFromFile("sprites.png", 16*16, 16*9, 16, 2, 7)
 }
 
@@ -62,15 +62,14 @@ func extractSubimageFromImage(img image.Image, fromx, fromy, w, h int) image.Ima
 	return subImg
 }
 
-func CreateAtlasFromFile(filename string, topleftx, toplefty, spriteSize int, createAllDirections bool,
-	totalFrames int) *spriteAtlas {
+func CreateAtlasFromFile(filename string, topleftx, toplefty, originalSpriteSize, desiredSpriteSize, totalFrames int, createAllDirections bool) *spriteAtlas {
 
 	file, _ := os.Open(filename)
 	img, _ := png.Decode(file)
 	file.Close()
 
 	newAtlas := spriteAtlas{
-		spriteSize:   spriteSize*int(SPRITE_SCALE_FACTOR),
+		spriteSize: originalSpriteSize *int(SPRITE_SCALE_FACTOR),
 	}
 	if createAllDirections {
 		newAtlas.atlas = make([][]rl.Texture2D, 4)
@@ -79,9 +78,9 @@ func CreateAtlasFromFile(filename string, topleftx, toplefty, spriteSize int, cr
 	}
 	// newAtlas.atlas
 	for currFrame := 0; currFrame < totalFrames; currFrame++ {
-		currPic := extractSubimageFromImage(img, topleftx+currFrame*spriteSize, toplefty, spriteSize, spriteSize)
+		currPic := extractSubimageFromImage(img, topleftx+currFrame*originalSpriteSize, toplefty, originalSpriteSize, originalSpriteSize)
 		rlImg := rl.NewImageFromImage(currPic)
-		rl.ImageResizeNN(rlImg, int32(spriteSize)*int32(SPRITE_SCALE_FACTOR), int32(spriteSize)*int32(SPRITE_SCALE_FACTOR))
+		rl.ImageResizeNN(rlImg, int32(desiredSpriteSize)*int32(SPRITE_SCALE_FACTOR), int32(desiredSpriteSize)*int32(SPRITE_SCALE_FACTOR))
 		newAtlas.atlas[0] = append(newAtlas.atlas[0], rl.LoadTextureFromImage(rlImg))
 		if createAllDirections {
 			for i := 1; i < 4; i++ {
@@ -153,7 +152,8 @@ func createAtlasFromRandomGenerated() *spriteAtlas {
 		0,
 		rnd.Rand(100)*24,
 		24,
-		true,
+		16,
 		4,
+		true,
 		)
 }
