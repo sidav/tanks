4c9000c620a7d57148e68b8916e68359c0b4c900
sidav <sidav94@gmail.com>
2022-01-28 03:50:35 +0300

    Makes build script better (includes current commit date, builds archives, clears after itself).

--- build_win.sh
+++ build_win.sh
@@ -1,1 +1,8 @@
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags="-s -w" -o tankzors.exe *.go
+mkdir -p build
+commitdate=$(git show -s --format=%ci | sed 's/[: -]/_/g' | sed 's/_+.*//g')
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags="-s -w" -o tankzors_$commitdate.exe *.go
+echo "Archive base name?"
+read basename
+zip build/$basename$commitdate.zip -r tankzors_$commitdate.exe assets/*
+rm tankzors_$commitdate.exe
+echo "Build successful."
